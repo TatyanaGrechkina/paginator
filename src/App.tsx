f20bec8cdccded84445db67bf084d9898afcfb6d
Tatyana Grechkina <t.grechkina@riberry.az>
2020-08-17 18:52:32 +0400

    add logic for scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.scss';
 import Paginator from './components/paginator/Paginator';
 
 function App() {
    const words = ['All', 'New', 'Popular'];
     return (
-        <Paginator pages={words} />
+        <div className={"App"}>
+         <Paginator pages={words} />
+        </div>
     );
 }
 
--- src/components/paginator/Paginator.tsx
+++ src/components/paginator/Paginator.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, createRef, RefObject, useRef} from 'react';
 import './Paginator.scss';
 import { ChevronLeftRounded, ChevronRightRounded } from '@material-ui/icons';
 import PaginatorItem from '../paginator-item/PaginatorItem';
@@ -7,32 +7,74 @@ interface Props {
     pages: string[];
 }
 
-class Paginator extends Component<Props, any> {
+interface State {
+    blockWidth: number;
+    selectedPage: string;
+    displayedPages: string[];
+}
 
+class Paginator extends Component<Props, State> {
     constructor(props: Props) {
         super(props);
+        this.state = {
+            blockWidth: 0,
+            selectedPage: props && props.pages && props.pages[1],
+            displayedPages: [],
+        }
     }
+
+     setPaginatorRefValues = () => {
+        const ref = document.getElementById("paginator__inner-content");
+        const blockWidth = ref?.clientWidth ?? 0;
+        const selectedFirstIndex = this.props.pages.indexOf(this.state.selectedPage);
+        const index = selectedFirstIndex >= 0 && selectedFirstIndex || 0;
+        let width = 0;
+        const displayElements: string[] = [];
+        this.props.pages.slice(index).forEach((page: string) => {
+            width += page.length * 15 + 32;
+            if (width < blockWidth) {
+                displayElements.push(page);
+            }
+        });
+
+        if (width < blockWidth && !!index) {
+            for (let ind = 0; ind < index; ind ++) {
+                const page = this.props.pages[index - ind - 1];
+                width += page.length * 15 + 32;
+                if (width < blockWidth) {
+                    displayElements.unshift(page);
+                }
+            }
+        }
+
+        this.setState({
+            blockWidth: blockWidth,
+            displayedPages: !!displayElements ? displayElements : [],
+        })
+    }
+
     componentDidMount() {
-        window.addEventListener('resize', this.handleResize)
+        window.addEventListener('resize', this.handleResize);
+        this.setPaginatorRefValues();
     }
-    handleResize(e) {
-        console.log(window.innerWidth);
+
+    handleResize = (e) => {
+        this.setPaginatorRefValues();
     }
+
     componentWillUnmount() {
         window.removeEventListener('resize', this.handleResize)
     }
 
     render () {
-        const {pages} = this.props;
+        const {displayedPages} = this.state;
         return (
-        <div className={'paginator'}>
         <div className={'paginator__content'}>
             <ChevronLeftRounded  className={'paginator__chevron'}/>
-            <div className={'paginator__elements'}>
-                {pages?.map((page) => <PaginatorItem page={page} />)}
+            <div className={'paginator__inner-content'}  id="paginator__inner-content">
+                {displayedPages?.map((page) => <PaginatorItem page={page} />)}
             </div>
             <ChevronRightRounded  className={'paginator__chevron'}/>
-        </div>
         </div>)
   } 
 }
